a6336e197feb7a82c9aca700ed96fc69711564ab
amirfarzamnia <amir2452fa@gmail.com>
2024-02-05 13:49:29 +0100

    Update

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import router from './router';
 import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faAddressCard, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faAddressCard, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
 library.add([faAddressCard, faGithub, faArrowUpRightFromSquare]);
 
