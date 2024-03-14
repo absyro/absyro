fb6fd2298a0ab2d39033aa28ccb27ed836d23783
amirfarzamnia <amir2452fa@gmail.com>
2024-03-14 14:01:56 +0100

    Added Copyright text

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     </div>
     <footer class="text-white py-4 px-4 md:px-8 border-t border-zinc-900">
         <div class="mx-auto text-center flex flex-col md:flex-row items-center justify-between gap-4">
-            <p class="border-b border-zinc-900 pb-4 md:border-0">&copy; 2024 Absyro - All rights reserved.</p>
+            <p class="border-b border-zinc-900 pb-4 md:border-0">Copyright &copy; 2024 Absyro - All rights reserved.</p>
             <div class="flex items-center gap-6">
                 <a target="_blank" href="https://instagram.com/theabsyro"><font-awesome-icon icon="fab fa-instagram" size="lg" class="hover:text-teal-400 duration-200" /></a>
                 <a target="_blank" href="https://github.com/absyro"><font-awesome-icon icon="fab fa-github" size="lg" class="hover:text-teal-400 duration-200" /></a>
