0a83b3e353bcd57a324bed67f29a4b5fdebb9dcc
Amir Farzamnia <102469398+amirfarzamnia@users.noreply.github.com>
2024-04-02 14:11:33 +0200

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col gap-4 py-4">
         <h1 class="text-2xl text-slate-400 mb-4">Absyro</h1>
-        <p>Absyro was founded in 2023 by Amir Farzamnia, Absyro is a technology company dedicated to creating next-generation technologies. Our mission is to solve problems and enhance everyday life with our unique and innovative solutions.</p>
+        <p>Absyro (Advanced Software Systems & Robotics Operations) was founded in 2023 by Amir Farzamnia, Absyro is a technology company dedicated to creating next-generation technologies. Our mission is to solve problems and enhance everyday life with our unique and innovative solutions.</p>
         <p>Our journey began with the creation of <a class="text-teal-400 hover:underline" href="https://botstudioo.com">Bot Studio</a>, a service for building robots. This marked the birth of Absyro - a company committed to developing unique products and services.</p>
         <p>We believe in accessibility and inclusivity. That's why most of our products and services are free to use, with optional low-cost items available for purchase. We do not restrict access based on location, gender, age, or financial status.</p>
         <p>At Absyro, we support the growth of the global tech community by making most of our products and services open-source. While some may initially be closed due to security and marketing considerations, we aim to make all our products open-source over time.</p>
