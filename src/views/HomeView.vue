2a93b4ce78563683db592b420191787c5cb5c5e2
amirfarzamnia <amir2452fa@gmail.com>
2024-04-08 09:30:35 +0200

    Fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@
                 <h2 class="text-2xl font-semibold">Bot Studio - Free, no-code bot maker</h2>
             </div>
             <hr class="my-4 border-zinc-900" />
-            <p class="mb-8">Bot Studio is a complimentary, no-code bot builder that enables developers to construct bots for various platforms including Telegram, WhatsApp, Discord, and more. It offers a user-friendly drag-and-drop interface for creating operational systems that can be deployed as effective chatbots.</p>
+            <p class="mb-8">Bot Studio is a complimentary, no-code bot builder that enables developers to construct bots for various platforms, including Telegram, WhatsApp, Discord, Slack, and more. It offers a user-friendly drag-and-drop editor interface for creating operational systems that can be deployed as effective robots.</p>
             <a href="https://www.botstudioo.com" class="text-teal-400 hover:underline"><font-awesome-icon icon="arrow-up-right-from-square" class="me-2" />Try Bot Studio for free</a>
         </div>
     </div>
